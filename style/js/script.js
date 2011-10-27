4f003bd1d72530d1b0001dfd501548d3ea6698e2
Matthias Kretschmann <krema@jpberlin.de>
2011-10-27 16:53:22 +0200

    Auswahl Loginkombination, pimped mit js für mehr Usability (radio select selektiert gesamte Reihe)

--- style/js/script.js
+++ style/js/script.js
@@ -3,24 +3,15 @@
 */
 
 
+$(document).ready(function() {
+  	
+  	// highlight the selected radiorow
+	$('.radiorow').filter(':has(:radio:selected)').end().click(function(event) {
+		if (event.target.type !== 'radio') {
+			$(':radio', this).trigger('click');
+		}
+		$('.radiorow').removeClass('selected');
+		$(this).toggleClass('selected');
+	});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
