b723011282f490654aa16d178e73efc0beb8da2e
Matthias Kretschmann <desk@kremalicious.com>
2012-02-07 14:31:22 +0100

    Neuer Prozess mit neuer Seite 2 & 3; entity encoding; im Text konsistent 'Nutzerkennzeichen' verwendet; js entfernt; meta viewport update

--- style/js/script.js
+++ style/js/script.js
@@ -1,17 +1,3 @@
 /* Author: 
 
 */
-
-
-$(document).ready(function() {
-  	
-  	// highlight the selected radiorow
-	$('.radiorow').filter(':has(:radio:selected)').end().click(function(event) {
-		if (event.target.type !== 'radio') {
-			$(':radio', this).trigger('click');
-		}
-		$('.radiorow').removeClass('selected');
-		$(this).toggleClass('selected');
-	});
-
-});
