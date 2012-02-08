015f6a7655e7af9e3f2593766e72559499060a38
Matthias Kretschmann <desk@kremalicious.com>
2012-02-08 18:53:41 +0100

    Page update

--- style/js/script.js
+++ style/js/script.js
@@ -1,3 +1,17 @@
-/* Author: 
+/* Author: Matthias Kretschmann
 
 */
+
+$(document).ready(function() {
+	
+	var $content = $('#emailCombination'),
+		$trigger = $('#emailCombinationTrigger');
+	
+	$content.hide();
+	
+	$trigger.click(function(){
+		$content.slideToggle();
+		return false;
+	});
+
+});
